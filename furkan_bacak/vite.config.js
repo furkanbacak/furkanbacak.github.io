5725c586e778fcbb628591b32f8a8056c6b2340f
Furkan <furkanbacak97@gmail.com>
2025-01-17 00:29:06 +0100

    new changes

--- furkan_bacak/vite.config.js
+++ furkan_bacak/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Use '/' because you're deploying to a custom domain
+  base: '/', // Use "/" if you're deploying to a custom domain
 });
