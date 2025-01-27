21973de447080cf4b1e98ed6805c4c5eb6935193
Furkan <furkanbacak97@gmail.com>
2025-01-27 13:18:15 +0100

    Added personal information and updated sections

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // Use "/" if you're deploying to a custom domain
-});
